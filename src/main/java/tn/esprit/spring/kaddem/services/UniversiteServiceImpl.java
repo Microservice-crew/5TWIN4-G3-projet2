3b5c586c241bcb3aef7c7e2c91a8b9fb407762be
arijhajji <61475226+arijhajji-1@users.noreply.github.com>
2023-10-27 13:31:55 +0100

    more bug fixes

--- src/main/java/tn/esprit/spring/kaddem/services/UniversiteServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/UniversiteServiceImpl.java
@@ -8,6 +8,7 @@ import tn.esprit.spring.kaddem.repositories.DepartementRepository;
 import tn.esprit.spring.kaddem.repositories.UniversiteRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -31,23 +32,40 @@ return  (universiteRepository.save(u));
      return  (universiteRepository.save(u));
     }
 
-  public Universite retrieveUniversite (Integer idUniversite){
-Universite u = universiteRepository.findById(idUniversite).get();
-return  u;
+    public Universite retrieveUniversite(Integer idUniversite) {
+        Optional<Universite> universiteOptional = universiteRepository.findById(idUniversite);
+
+        return universiteOptional.orElse(null);
     }
+
     public  void deleteUniversite(Integer idUniversite){
         universiteRepository.delete(retrieveUniversite(idUniversite));
     }
 
-    public void assignUniversiteToDepartement(Integer idUniversite, Integer idDepartement){
-        Universite u= universiteRepository.findById(idUniversite).orElse(null);
-        Departement d= departementRepository.findById(idDepartement).orElse(null);
-        u.getDepartements().add(d);
-        universiteRepository.save(u);
+    public void assignUniversiteToDepartement(Integer idUniversite, Integer idDepartement) {
+        // Code non couvert
+
+        Universite u = universiteRepository.findById(idUniversite).orElse(null);
+        Departement d = departementRepository.findById(idDepartement).orElse(null);
+
+        if (u != null && d != null) {
+            u.getDepartements().add(d);
+            universiteRepository.save(u);
+        }
     }
 
-    public Set<Departement> retrieveDepartementsByUniversite(Integer idUniversite){
-Universite u=universiteRepository.findById(idUniversite).orElse(null);
-return u.getDepartements();
+
+    public Set<Departement> retrieveDepartementsByUniversite(Integer idUniversite) {
+
+
+        Universite u = universiteRepository.findById(idUniversite).orElse(null);
+
+        if (u != null) {
+            return u.getDepartements();
+        }else{
+            return null;
+        }
     }
+
+
 }

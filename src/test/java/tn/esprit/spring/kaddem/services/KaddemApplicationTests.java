9717db04bc539ad664f052a32b6d8b856dcc937d
jawhar-bali <jawhar.bali@esprit.tn>
2023-11-02 23:38:02 +0100

    Test

--- src/test/java/tn/esprit/spring/kaddem/services/KaddemApplicationTests.java
+++ src/test/java/tn/esprit/spring/kaddem/services/KaddemApplicationTests.java
@@ -1,113 +1,106 @@
-package tn.esprit.spring.kaddem.services;
+package tn.esprit.spring.kaddem;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.junit.jupiter.api.Assertions;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
-import tn.esprit.spring.kaddem.entities.Contrat;
-import tn.esprit.spring.kaddem.entities.Specialite;
-import tn.esprit.spring.kaddem.repositories.ContratRepository;
+import tn.esprit.spring.kaddem.entities.Etudiant;
+import tn.esprit.spring.kaddem.entities.Option;
+import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
+import tn.esprit.spring.kaddem.services.EtudiantServiceImpl;
+import tn.esprit.spring.kaddem.services.IEtudiantService;
 
-
-import static org.mockito.Mockito.*;
-
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
-
-
-
-@ContextConfiguration(classes = {ContratServiceImpl.class})
+@SpringBootTest
+@ContextConfiguration(classes = {EtudiantServiceImpl.class})
 @ExtendWith(MockitoExtension.class)
-
- class KaddemApplicationTests {
-    @Mock
-    private ContratRepository contratRepository;
+public class KaddemApplicationTests {
+    @MockBean
+    private EtudiantRepository etudiantRepository;
 
     @InjectMocks
-    private ContratServiceImpl contratService;
+    private EtudiantServiceImpl etudiantService;
 
+    @Autowired
+    IEtudiantService etudiant;
 
-     // Initialize the mocks
-    @BeforeEach
-    public void init() {
-        MockitoAnnotations.initMocks(this);
+    @Test
+    void testRetrieveAllEtudiants() {
+        List<Etudiant> actualRetrieveAllEtudiantsResult = this.etudiantService.retrieveAllEtudiants();
+        Assertions.assertEquals(0, actualRetrieveAllEtudiantsResult.size());
     }
 
     @Test
-    void testRetrieveAllContrats() {
-        List<Contrat> mockContats = new ArrayList<>();
-        mockContats.add(new Contrat(1, new Date(2023, 10, 21), new Date(2023, 10, 21), Specialite.IA, false, 1));
-        mockContats.add(new Contrat(2, new Date(2023, 10, 21), new Date(2023, 10, 21), Specialite.IA, false, 1));
-        when(contratRepository.findAll()).thenReturn(mockContats);
-        List<Contrat> result = contratService.retrieveAllContrats();
-        Assertions.assertEquals(2, result.size());
-        Assertions.assertEquals(mockContats, result);
+    void testAddEtudiant() {
+        Etudiant etudiant = new Etudiant();
+        etudiant.setIdEtudiant(1);
+        etudiant.setNomE("John Doe");
+        etudiant.setPrenomE("Doe");
+        etudiant.setOp(Option.GAMIX);
+
+        when(etudiantRepository.save(any())).thenReturn(etudiant);
+
+        Etudiant etudiant1 = new Etudiant();
+        etudiant1.setIdEtudiant(1);
+        etudiant1.setNomE("John Doe");
+        etudiant1.setPrenomE("Doe");
+        etudiant1.setOp(Option.GAMIX);
+
+        assertSame(etudiant, etudiantService.addEtudiant(etudiant1));
+        verify(etudiantRepository).save(any());
     }
-
-  //  @Test
- /*   void testAddContrat() {
-        Contrat contrat = new Contrat();
-        contrat.setIdContrat(1);
-        contrat.setDateDebutContrat(new Date(2023, 10, 21));
-        contrat.setDateFinContrat(new Date(2023, 10, 21));
-        contrat.setSpecialite(Specialite.IA);
-        contrat.setArchive(false);
-        contrat.setMontantContrat(1);
-        when(contratRepository.save(
-                (Contrat) any())
-        ).thenReturn(contrat);
-        Contrat actualAddContactResult = contratService.addContrat(new Contrat());
-        assertSame(contrat, actualAddContactResult);
-        verify(contratRepository).save((Contrat) any());
-    }*/
-
-
-
-  /*  @Test
-    void testUpdateContrat() {
-        Contrat contrat = new Contrat();
-        contrat.setIdContrat(1);
-        contrat.setDateDebutContrat(new Date(2023, 10, 21));
-        contrat.setDateFinContrat(new Date(2023, 10, 21));
-        contrat.setSpecialite(Specialite.IA);
-        contrat.setArchive(false);
-        contrat.setMontantContrat(1);
-        when(contratRepository.save(
-                (Contrat) any())
-        ).thenReturn(contrat);
-        Contrat actualUpdateContactResult = contratService.updateContrat(new Contrat());
-        assertSame(contrat, actualUpdateContactResult);
-        verify(contratRepository).save((Contrat) any());
-    }
-
 
     @Test
-    void testRetrieveContrat() {
-        Contrat contrat = new Contrat();
-        contrat.setIdContrat(1);
-        contrat.setDateDebutContrat(new Date(2023, 10, 21));
-        contrat.setDateFinContrat(new Date(2023, 10, 21));
-        contrat.setSpecialite(Specialite.IA);
-        contrat.setArchive(false);
-        contrat.setMontantContrat(1);
-        when(contratRepository.findById((Integer) any())).thenReturn(Optional.<Contrat>of(contrat));
-        Contrat actualRetrieveContactResult = contratService.retrieveContrat(1);
-        assertSame(contrat, actualRetrieveContactResult);
-        verify(contratRepository).findById((Integer) any());
-    }*/
+    void testDeleteEtudiant() {
+        etudiantRepository.deleteById(1);
+        verify(etudiantRepository).deleteById(1);
+    }
 
+    @Test
+    void testUpdateEtudiant() {
+        Etudiant etudiant = new Etudiant();
+        etudiant.setIdEtudiant(1);
+        etudiant.setNomE("Nom");
+        etudiant.setPrenomE("Prenom");
+        etudiant.setOp(Option.SIM);
 
+        when(etudiantRepository.save(any())).thenReturn(etudiant);
 
+        Etudiant etudiant1 = new Etudiant();
+        etudiant1.setIdEtudiant(1);
+        etudiant1.setNomE("Nom");
+        etudiant1.setPrenomE("Prenom");
+        etudiant1.setOp(Option.SIM);
 
+        assertSame(etudiant, etudiantService.updateEtudiant(etudiant1));
+        verify(etudiantRepository).save(any());
+    }
 
+    @Test
+    void testRetrieveEtudiant() {
+        Etudiant etudiant = new Etudiant();
+        etudiant.setIdEtudiant(1);
+        etudiant.setNomE("John Doe");
+        etudiant.setPrenomE("Doe");
+        etudiant.setOp(Option.GAMIX);
+
+        Optional<Etudiant> ofResult = Optional.of(etudiant);
+
+        when(etudiantRepository.findById(anyInt())).thenReturn(ofResult);
+
+        Etudiant retrievedEtudiant = etudiantService.retrieveEtudiant(1);
+
+        verify(etudiantRepository).findById(1);
+
+        assertSame(etudiant, retrievedEtudiant);
+    }
 }
